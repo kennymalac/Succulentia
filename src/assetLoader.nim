dfb961347f5bc1a198c2a75886b2b143102cf86c
Kenneth Malac <kennethm777@gmail.com>
2019-10-05 12:48:23 -0700

    Added default graphics location to assetLoader newImageAsset and dummy load procedure

--- src/assetLoader.nim
+++ src/assetLoader.nim
@@ -16,7 +16,7 @@ proc newAssetLoader*(location: string, scale: Vector2f = vec2(1.0, 1.0)): AssetL
   result = AssetLoader(location: location)
 
 proc newImageAsset*(self: AssetLoader, location: string, size: Vector2i): ImageAsset =
-  result = ImageAsset(texture: new_Texture(fmt"{self.location}/{location}"), size: size)
+  result = ImageAsset(texture: new_Texture(fmt"{self.location}/graphics/{location}"), size: size)
 
 proc newSprite*(self: AssetLoader, image: ImageAsset): Sprite =
   result = new_Sprite(image.texture)
