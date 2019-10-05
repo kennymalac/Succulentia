843a2573d51ef22f4c551edbf9e4a54d4da44630
rhenvar <garrickkrol@hotmail.com>
2019-10-05 09:49:18 -0700

    Entity draw and update stubs

--- src/entities/entity.nim
+++ src/entities/entity.nim
@@ -2,14 +2,19 @@ import csfml
 
 type
   Entity* = ref object of RootObj
-    position*: Vector2i
     sprite*: Sprite
 
-proc move*(self: Entity, position: Vector2i) =
-  self.position = position
-
-proc newEntity*(self: Entity, sprite: Sprite) =
+proc newEntity*(self: Entity, sprite: Sprite, ) =
   self.sprite = sprite
+
+proc update*(self: Entity) =
+  discard
+
+proc draw() =
+  discard
+
+proc move*(self: Entity, position: Vector2i) =
+  discard
 
 proc print*(self: Entity) =
   echo "I am an entity\n"
