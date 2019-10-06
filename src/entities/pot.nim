5406e7fcdeb783b2240341746346cf2bb073e11e
rhenvar <garrickkrol@hotmail.com>
2019-10-05 18:07:25 -0700

    Pot

---
+++ src/entities/pot.nim
@@ -0,0 +1,18 @@
+import csfml
+import entity, succulent
+
+type
+  Pot* = ref object of Entity
+    health*: int 
+    suc*: Succulent
+
+proc newPot*(self: Pot, sprite: Sprite): Pot =
+  result = Pot(health: 100)
+  initEntity(result, sprite)
+
+# pre-planted pot
+proc newPot*(self: Pot, sprite: Sprite, suc: Succulent): Pot =
+  result = Pot(health: 100)
+  result.suc = suc
+  initEntity(result, sprite)
+
